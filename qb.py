ecf33081140bca654b6879c43261794e718bdbc8
zhufeng <iwantcomputer@qq.com>
2020-04-04 14:36:07 +0000

    重构：不同功能函数化模块化

--- qb.py
+++ qb.py
@@ -1,90 +1,127 @@
-import qbittorrentapi
+import os
 import re
-
-## 读取qb_auto_ext.txt文件中定义的qb webui的登录信息
-userFile = open("qb_auto_ext.txt" , "r");
-userLines = userFile.read().splitlines();
-# print (userLines);
-# host = userLines[3];
-host = userLines[0];
-user = userLines[1];
-passwd = userLines[2];
-# print (userLines);
-print ({host});
-print ({user});
-userFile.close();
+import configparser
+import qbittorrentapi
 
 
-## 读取qb_categories.txt文件中定义的
-## tracker名和分类名的对应关系，并生成字典
-cateFile = open("qb_categories.txt" , "r");
-# cateLines = cateFile.read().splitlines();
-# print (cateLines);
-cateDict = {};
+def readConf():
+    print ("I'm readConf()...");
 
-while True:
-    line = cateFile.readline().split("\n")[0];
-    # print (line);
-    if line == '':
-        break
-    index = line.find(':');
-    # print (index);
-    key = line[:index];
-    value = line[index+1:];
-    cateDict[key] = value;
+    global host;
+    global user;
+    global passwd;
+    global cateDict;
+    global labelDict;
 
-cateFile.close();
-# print (cateDict.items());
-# for k,v in cateDict.items():
-    # print (k, "=", v);
+    cfg = configparser.ConfigParser();
+    if os.path.exists('qb_auto1.ini'):
+        cfg.read_file(open("qb_auto1.ini"));
+        print ("Reading qb_auto1.ini...");
+    else:
+        cfg.read_file(open("qb_auto.ini"));
+        print ("Reading qb_auto.ini...");
+    # print (cfg.sections());
 
+    # 读取配置文件中qb webui[connection]登录信息部分
+    host = cfg['connection']['localhost'];
+    # host = cfg['connection']['host']);
+    user = cfg['connection']['user'];
+    passwd = cfg['connection']['password'];
 
-# instantiate a Client using the appropriate WebUI configuration
-# qbt_client = qbittorrentapi.Client(host='localhost:****', username='***', password='***')
-qbt_client = qbittorrentapi.Client(host=host, username=user, password=passwd);
+    # 读取配置文件中[category]分类部分
+    cateDict = cfg._sections['category'];
+    # print (cateDict);
 
-# the Client will automatically acquire/maintain a logged in state in line with any request.
-# therefore, this is not necessary; however, you many want to test the provided login credentials.
-try:
-    qbt_client.auth_log_in()
-except qbittorrentapi.LoginFailed as e:
-    print(e);
+    # 读取配置文件中[label]标签部分
+    labelDict = cfg._sections['label'];
+    # print (labelDict);
 
-# display qBittorrent info
-print(f'qBittorrent: {qbt_client.app.version}');
-print(f'qBittorrent Web API: {qbt_client.app.web_api_version}');
-# for k,v in qbt_client.app.build_info.items():
-    # print(f'{k}: {v}');
-
-# retrieve and show all torrents
-# for torrent in qbt_client.torrents_info():
-    # print(f'{torrent.hash[-6:]}: {torrent.name} ({torrent.state})')
-
-# pause all torrents 
-# # # # # qbt_client.torrents.pause.all()
-
-# print (qbt_client.torrents_info(status_filter='active'));
-
-# for torrent in qbt_client.torrents_info():
-for torrent in qbt_client.torrents_info(category=''):
-# for torrent in qbt_client.torrents_info(category='frds'):
-    # print(f'{torrent.hash[-6:]}: {torrent.name} ({torrent.state})')
-    # print (torrent);
-    for tracker in torrent.trackers:
-        trackerUrl = tracker.url[0:30];
-        if (trackerUrl.startswith('http')):
-            # print (trackerUrl);
-            # print (torrent.name);
-            for k,v in cateDict.items():
-                # print (k);
-                if re.search(k, trackerUrl):
-                    # print (v)
-                    torrent.set_category(category=v);
-                    # print ("Category SET!! -> " + torrent.name[0:30]);
-                    print (torrent.name[0:30] + " -> " + v );
+    print ("");
 
 
+def qbConn(host,user,passwd):
+    print ("I'm qbConn()...");
+
+    global qbClient;
+
+    # instantiate a Client using the appropriate WebUI configuration
+    # qbClient = qbittorrentapi.Client(host='localhost:****', username='***', password='***')
+    qbClient = qbittorrentapi.Client(host=host, username=user, password=passwd);
+
+    # the Client will automatically acquire/maintain a logged in state in line with any request.
+    # therefore, this is not necessary; however, you many want to test the provided login credentials.
+    try:
+        qbClient.auth_log_in()
+    except qbittorrentapi.LoginFailed as e:
+        print(e);
+
+    # display qBittorrent info
+    print(f'qBittorrent: {qbClient.app.version}');
+    print(f'qBittorrent Web API: {qbClient.app.web_api_version}');
+    # for k,v in qbClient.app.build_info.items():
+        # print(f'{k}: {v}');
+
+    # retrieve and show all torrents
+    # for torrent in qbClient.torrents_info():
+        # print(f'{torrent.hash[-6:]}: {torrent.name} ({torrent.state})')
+
+    # pause all torrents 
+    # # # # # qbClient.torrents.pause.all()
+
+    # print (qbClient.torrents_info(status_filter='active'));
+
+    print ("");
 
 
+def autoCate(qbClient):
+    print ("I'm autoCate()...");
+
+    # for torrent in qbClient.torrents_info():
+    for torrent in qbClient.torrents_info(category=''):
+        # for torrent in qbtCon.torrents_info(category='frds'):
+        # print(f'{torrent.hash[-6:]}: {torrent.name} ({torrent.state})')
+        # print (torrent);
+        for tracker in torrent.trackers:
+            trackerUrl = tracker.url[0:30];
+            if (trackerUrl.startswith('http')):
+                # print (trackerUrl);
+                # print (torrent.name);
+                for k,v in cateDict.items():
+                    # print (k);
+                    if re.search(k, trackerUrl):
+                        # print (v)
+                        torrent.set_category(category=v);
+                        # print ("Category SET!! -> " + torrent.name[0:30]);
+                        print (torrent.name[0:30] + " -> " + v );
+
+    print ("AutoCate done...!", "\n");
+
+
+def autoLabel(qbClient):
+    pass;
+
+
+def forceReport():
+    pass;
+
+
+def main():
+    print ("I'm main()...", "\n");
+
+    global host;
+    global user;
+    global passwd;
+    global cateDict;
+    global labelDict;
+    global qbClient;
+
+    readConf();
+    # print (host,user,passwd,cateDict,labelDict);
+    qbConn(host, user, passwd);
+    autoCate(qbClient);
+
+
+if __name__ == '__main__':
+    main();
 
 
---
+++ qb_auto.bat
@@ -0,0 +1,4 @@
+d:
+cd D:\³£ÓÃÈí¼ş\qbittorrent_x64\qb_auto
+python qb.py
+pause
